58c55494fbfc7eb99197b828f4ba511aaa96fb08
fontenele <henriquedevb@gmail.com>
2022-02-17 20:06:29 -0300

    feat: add controller message

--- controller/src/app.controller.ts
+++ controller/src/app.controller.ts
@@ -1,14 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-//ponto de partida da api e como se fosse um component
-
-// /app virou a rota inicial
-@Controller('/app')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  //metodos
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- controller/src/app.service.ts
+++ controller/src/app.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@nestjs/common';
 
-//funcao de banco de dados e algo do tipo vao ficar aqui
-
 @Injectable()
 export class AppService {
   getHello(): string {
