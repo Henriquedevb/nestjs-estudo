fad2631d9e319691c3ce2c90dbb1627c25990e4e
fontenele <henriquedevb@gmail.com>
2022-02-17 20:39:03 -0300

    feat: add module messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessagesController } from './messages/messages.controller';
-import { MessagesService } from './messages/messages.service';
+import { MessagesModule } from './messages/messages.module';
 
 //ponto de partida
 
 @Module({
-  imports: [],
-  controllers: [AppController, MessagesController],
-  providers: [AppService, MessagesService],
+  imports: [MessagesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
